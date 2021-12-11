ebb965520a44bd69193b0840059ff23bd2c7432a
Ilya-katovich <92888188+Ilya-katovich@users.noreply.github.com>
2021-12-11 23:53:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,45 +2,38 @@ import requests
 from datetime import datetime
 import telebot
 from auth_data import token
-
+import webbrowser
 
 def get_data():
-    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-    response = req.json()
-    sell_price = response["btc_usd"]["sell"]
-    print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
+    req = webbrowser.open_new_tab('https://myfin.by/api/currency/minsk')
+    print(req)
 
 
 def telegram_bot(token):
     bot = telebot.TeleBot(token)
 
-    @bot.message_handler(commands=["start"])
+    @bot.message_handler(commands=["HI"])
     def start_message(message):
-        bot.send_message(message.chat.id, "Hello friend! Write the 'price' to find out the cost of BTC!")
+        bot.send_message(message.chat.id, "Hello Boss!")
 
     @bot.message_handler(content_types=["text"])
     def send_text(message):
-        if message.text.lower() == "price":
+        if message.text.lower() == "many":
             try:
-                req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
-                response = req.json()
-                sell_price = response["btc_usd"]["sell"]
-                bot.send_message(
-                    message.chat.id,
-                    f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}"
-                )
+                req = webbrowser.open_new_tab('https://myfin.by/api/currency/minsk')
+
+
             except Exception as ex:
                 print(ex)
                 bot.send_message(
                     message.chat.id,
-                    "Damn...Something was wrong..."
+                    "AAAAAAAAAAA"
                 )
         else:
-            bot.send_message(message.chat.id, "Whaaat??? Check the command dude!")
+            bot.send_message(message.chat.id, "Whaaat?Boss!")
 
     bot.polling()
 
 
 if __name__ == '__main__':
-    # get_data()
     telegram_bot(token)
